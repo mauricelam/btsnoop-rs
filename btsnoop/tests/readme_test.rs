72cbf367414aaba77fa824f7094ea78eb460192e
Maurice Lam <me@mauricelam.com>
2023-04-05 00:29:04 -0700

    Update btsnoop version in readme, and add test to make sure it's not stale

---
+++ btsnoop/tests/readme_test.rs
@@ -0,0 +1,5 @@
+#[test]
+fn test_btsnoop_version() {
+    let readme = include_str!("../../README.md");
+    assert!(readme.contains(format!(r#"btsnoop = "{}""#, env!("CARGO_PKG_VERSION")).as_str()));
+}
