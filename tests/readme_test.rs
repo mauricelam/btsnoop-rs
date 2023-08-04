3f1b4af7450f87a1d0f36627273349d2d98629a8
Maurice Lam <me@mauricelam.com>
2023-08-03 21:46:53 -0700

    Fix README path

--- tests/readme_test.rs
+++ tests/readme_test.rs
@@ -1,5 +1,5 @@
 #[test]
 fn test_btsnoop_version() {
-    let readme = include_str!("../../README.md");
+    let readme = include_str!("../README.md");
     assert!(readme.contains(format!(r#"btsnoop = "{}""#, env!("CARGO_PKG_VERSION")).as_str()));
 }
